ce259dc16893dd139dd1c330dd2d470bce8d4fd1
marianadiaz12 <mariana.diazes@gmail.com>
2016-10-19 17:04:53 -0500

    Versión final

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -3,9 +3,9 @@ function validateForm(){
 	var elApellido = document.getElementById("lastname").value;
 	var elEmail = document.getElementById("input-email").value;
 	var elPassword = document.getElementById("input-password").value;
-  var elSelec = document.getElementsByTagName("select")[0];
-  var reexp = /^[A-Z]/;
-
+	var elSelec = document.getElementsByTagName("select")[0];
+	var reexp = /^[A-Z]/;
+	var expemail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 	if(elNombre == null || elNombre.length == 0 || reexp.test (elNombre) == false){
 				alert('Debe ingresar un nombre,  1ra letra en mayúscula');
@@ -17,32 +17,31 @@ function validateForm(){
 				return false;
 			}
 
-  var expemail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  if (elEmail == null || expemail.test(elEmail) == false) {
+ 
+  	if (elEmail == null || expemail.test(elEmail) == false) {
 	 			alert('Debe escribir un correo válido');
 	 			return false;
 			}
 	
 	if (elPassword.length < 6 ) {
 		alert('La contraseña debe ser minimo de 6 caracteres');
-			return false;
-				}
+		return false;
+		}
     else if(elPassword == 123456){
-      alert('La contraseña no puede ser 123456');
-      return false;
+      	alert('La contraseña no puede ser 123456');
+      	return false;
     }
     else if(elPassword == 098765){
-      alert('La contraseña no puede ser 098765');
-      return false;
+      	alert('La contraseña no puede ser 098765');
+      	return false;
     }
     else if(elPassword == "password"){
-      alert('La contraseña no puede ser password');
-      return false;
+	    alert('La contraseña no puede ser password');
+	    return false;
     }
 
-  if (elSelec.value == "0") {
-    alert('Debes seleccionar al menos una opción');
-    return false;
-  }
-
+  	if (elSelec.value == "0") {
+    	alert('Debes seleccionar al menos una opción');
+    	return false;
+  	}
 }
