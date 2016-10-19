ab7af6dd36a0e4dacb2dcc10e6eec72a37ee2e89
marianadiaz12 <mariana.diazes@gmail.com>
2016-10-19 16:56:55 -0500

    Versión final del ejercicio

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,32 +1,48 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-var elNombre = document.getElementById("name").value;
-var elApellido = document.getElementById("lastname").value;
-var elEmail = document.getElementById("input-email");
-var elPassword = document.getElementById("input-password").value;
+	var elNombre = document.getElementById("name").value;
+	var elApellido = document.getElementById("lastname").value;
+	var elEmail = document.getElementById("input-email").value;
+	var elPassword = document.getElementById("input-password").value;
+  var elSelec = document.getElementsByTagName("select")[0];
+  var reexp = /^[A-Z]/;
 
-if(elNombre == null || elNombre.length == 0 || /^\s+$/.test(elNombre)){
-			alert('ERROR: El campo nombre no debe ir vacío');
-			return false;
-		}
-if(elApellido == null || elApellido.length == 0 || /^\s+$/.test(elApellido)){
-			alert('ERROR: El campo Apellido no debe ir vacío');
-			return false;
-			elApellido.toUpperCase(0);
-		}
 
-/* Ya no pude validar el correo 
-if(!(/\S+@\S+\.\S+/.test(elEmail))){
- 			alert('ERROR: Debe escribir un correo válido');
- 			return false;
-		}
-*/
-
-if (elPassword == 123456 || elPassword.length < 6 || elPassword == "password" || elPassword= 0987654) {
-	alert('La contraseña no es minimo de 6 caracteres o no es valida');
-		return false;
+	if(elNombre == null || elNombre.length == 0 || reexp.test (elNombre) == false){
+				alert('Debe ingresar un nombre,  1ra letra en mayúscula');
+				return false;
 			}
 
+	if(elApellido == null || elApellido.length == 0 || reexp.test (elNombre) == false){
+				alert('Debe ingresar su apellido, 1ra letra en mayúscula');
+				return false;
+			}
+
+  var expemail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  if (elEmail == null || expemail.test(elEmail) == false) {
+	 			alert('Debe escribir un correo válido');
+	 			return false;
+			}
+	
+	if (elPassword.length < 6 ) {
+		alert('La contraseña debe ser minimo de 6 caracteres');
+			return false;
+				}
+    else if(elPassword == 123456){
+      alert('La contraseña no puede ser 123456');
+      return false;
+    }
+    else if(elPassword == 098765){
+      alert('La contraseña no puede ser 098765');
+      return false;
+    }
+    else if(elPassword == "password"){
+      alert('La contraseña no puede ser password');
+      return false;
+    }
+
+  if (elSelec.value == "0") {
+    alert('Debes seleccionar al menos una opción');
+    return false;
+  }
 
 }
-
